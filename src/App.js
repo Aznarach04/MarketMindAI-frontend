2e052f87e0aae45321435fae51dbb2b9114dc9b3
Aznarach04 <rachid.ennaciri.38@gmail.com>
2025-02-13 11:54:26 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur MarketMindAI</h1>
+      <p>Votre analyse de marché commence ici.</p>
+    </div>
+  );
+}
+
+export default App;
